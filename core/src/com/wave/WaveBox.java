687a0e1f4597d4e347f84e635ffe9db2411bb4ba
koreanwglasses <koreanwglasses@gmail.com>
2017-05-22 23:37:46 -0400

    Added force function

--- core/src/com/wave/WaveBox.java
+++ core/src/com/wave/WaveBox.java
@@ -5,22 +5,25 @@ package com.wave;
  */
 public class WaveBox {
     private double[][] z; // x, y (col major) array
+    private double mass = 1;
     private double k = 1;
+    private int resolution;
 
     public WaveBox(int resolution) {
-        z = new double[resolution][resolution];
+        this.resolution = resolution;
+        this.z = new double[resolution][resolution];
     }
 
     public void step(double dt) {
 
     }
 
-    private double force(int xc, int yc) {
+    private double force(int xi, int yi) {
         // cross based
-        double dz1 = (z[xc + 1][yc] - z[xc][yc]);
-        double dz2 = (z[xc - 1][yc] - z[xc][yc]);
-        double dz3 = (z[xc][yc + 1] - z[xc][yc]);
-        double dz4 = (z[xc][yc - 1] - z[xc][yc]);
+        double dz1 = xi < resolution - 1 ? (z[xi + 1][yi] - z[xi][yi]) : 0; // If neighbor exists, then dz = neighbor - z, else 0
+        double dz2 = xi > 0 ? (z[xi - 1][yi] - z[xi][yi]): 0;
+        double dz3 = yi < resolution - 1 ? (z[xi][yi + 1] - z[xi][yi]) : 0;
+        double dz4 = yi > 0 ? (z[xi][yi - 1] - z[xi][yi]) : 0;
 
         return k * (dz1 + dz2 + dz3 + dz4);
     }
